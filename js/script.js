6f32da02831c297f853e38d5b386d19091109e55
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-24 19:47:42 +0500

    Add addBook functionality in script.js

--- js/script.js
+++ js/script.js
@@ -9,16 +9,45 @@ const booksArray = [
   { id: 3, title: 'Title3', author: 'Author3' },
 ];
 
-let itemContent = '';
-booksArray.forEach((book) => {
-  itemContent += `
-  <li class="w-50 mx-auto">
-    <div>
-      <h3>${book.title}</h3>
-      <h4>${book.author}</h4>
-      <button type="button" id="${book.id}">Remove</button>
-    </div>
-    <hr>
-  </li>`;
+// Display Books
+function displayBooks(){
+  let itemContent = '';
+  booksArray.forEach((book) => {
+    itemContent += `
+    <li class="w-50 mx-auto">
+      <div>
+        <h3>${book.title}</h3>
+        <h4>${book.author}</h4>
+        <button type="button" id="${book.id}">Remove</button>
+      </div>
+      <hr>
+    </li>`;
+  });
+  booksContainer.innerHTML = itemContent;
+}
+
+displayBooks();
+
+const titleInput = document.querySelector('#titleInput');
+const authorInput = document.querySelector('#authorInput');
+const addBtn = document.querySelector('#addBtn');
+
+
+//Add Book
+function addBooks(title, author){
+  const bookItem ={
+    id: Math.floor(Math.random()*1000),
+    title: title.value,
+    author: author.value
+  };
+
+  booksArray.push(bookItem);
+  displayBooks();
+}
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBooks(titleInput, authorInput);
 });
-booksContainer.innerHTML = itemContent;
+
+
