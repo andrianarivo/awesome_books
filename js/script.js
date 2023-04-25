6793b36702b686ae8168312d9207f73c86e84f2f
andrianarivo <andrianarivo.david@gmail.com>
2023-04-25 11:17:32 +0300

    build localstorage and fix remove book

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 const booksContainer = document.querySelector('.books');
-let booksArray = [
-  {
-    id: 1,
-    title: 'Title1',
-    author: 'Author1',
-  },
-  { id: 2, title: 'Title2', author: 'Author2' },
-  { id: 3, title: 'Title3', author: 'Author3' },
-];
+let booksArray = JSON.parse(localStorage.getItem('books')) || [];
 
 // Display Books
 function displayBooks() {
@@ -24,41 +16,41 @@ function displayBooks() {
     </li>`;
   });
   booksContainer.innerHTML = itemContent;
+
+  // Remove Book
+  const deleteBtns = document.querySelectorAll('.remove-btn');
+  function removeBook(id) {
+    booksArray = booksArray.filter((book) => book.id.toString() !== id);
+    localStorage.setItem('books', JSON.stringify(booksArray));
+    displayBooks();
+  }
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', (e) => {
+      removeBook(e.target.id);
+    });
+  });
 }
 
 displayBooks();
-
 const titleInput = document.querySelector('#titleInput');
 const authorInput = document.querySelector('#authorInput');
 const addBtn = document.querySelector('#addBtn');
 
-//Add Book
+// Add Book
 function addBooks(title, author) {
   const bookItem = {
     id: Math.floor(Math.random() * 1000),
     title: title.value,
     author: author.value,
   };
-
   booksArray.push(bookItem);
+  localStorage.setItem('books', JSON.stringify(booksArray));
   displayBooks();
+  document.getElementById('titleInput').value = '';
+  document.getElementById('authorInput').value = '';
 }
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addBooks(titleInput, authorInput);
-});
-
-// Remove Book
-const deleteBtns = document.querySelectorAll('.remove-btn');
-
-function removeBook(id) {
-  booksArray = booksArray.filter((book) => book.id.toString() !== id);
-  displayBooks();
-}
-
-deleteBtns.forEach((deleteBtn) => {
-  deleteBtn.addEventListener('click', (e) => {
-    removeBook(e.target.id);
-  });
 });
