ca46f46cc94b74baf34ad5cb019292ae20933d8a
Andrianarivo David <andrianarivo.david@gmail.com>
2023-04-25 20:40:50 +0300

    html validation

--- js/script-class.js
+++ js/script-class.js
@@ -39,11 +39,15 @@ class BookStore {
     this.booksArray.forEach((book) => {
       content += `
     <li class="w-100 mx-auto list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">
-        <h5>"${book.title}" by ${book.author}</h5>
-        <button type="button" id="${book.id}" class="remove-btn border border-dark border-3 bg-danger text-light rounded">Remove</button>
+      <h5>"${book.title}" by ${book.author}</h5>
+      <button type="button" id="${book.id}" class="remove-btn border border-dark border-3 bg-danger text-light rounded">Remove</button>
     </li>`;
     });
     return content;
+  }
+  // Check if empty
+  empty() {
+    return this.booksArray.length === 0;
   }
 }
 
@@ -51,18 +55,40 @@ const bookStore = new BookStore();
 bookStore.getData();
 booksContainer.innerHTML = bookStore.render();
 
+// ADD A NEW BOOK
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  const titleValue = titleInput.value;
+  const authorValue = authorInput.value;
+  titleInput.className = '';
+  authorInput.className = '';
+
+  if (titleValue === '' && authorValue === '') {
+    titleInput.className = 'border border-2 border-danger';
+    authorInput.className = 'border border-2 border-danger';
+    return;
+  } else if (authorValue === '') {
+    authorInput.className = 'border border-2 border-danger';
+    return;
+  } else if (titleValue === '') {
+    titleInput.className = 'border border-2 border-danger';
+    return;
+  }
   const book = new Book(titleInput.value, authorInput.value);
   bookStore.addBook(book);
   bookStore.saveData();
   booksContainer.innerHTML = bookStore.render();
+  booksContainer.classList.add('border');
 });
 
+// REMOVE A BOOK
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove-btn')) {
     bookStore.remove(e.target.id);
     bookStore.saveData();
     booksContainer.innerHTML = bookStore.render();
+    if (bookStore.empty()) {
+      booksContainer.classList.remove('border');
+    }
   }
 });
