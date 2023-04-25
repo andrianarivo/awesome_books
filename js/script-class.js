beacd5e5a2e964654aee452644aed2e38784cacf
andrianarivo <andrianarivo.david@gmail.com>
2023-04-25 16:44:46 +0300

    updated BookStore

--- js/script-class.js
+++ js/script-class.js
@@ -2,22 +2,45 @@ const titleInput = document.querySelector('#titleInput');
 const authorInput = document.querySelector('#authorInput');
 const addBtn = document.querySelector('#addBtn');
 const booksContainer = document.querySelector('.books');
-let booksArray = JSON.parse(localStorage.getItem('books')) || [];
 
 class Book {
   constructor(title, author) {
+    this.id = Math.floor(Math.random() * 1000);
     this.title = title;
     this.author = author;
   }
 }
 
 class BookStore {
-  constructor() {}
+  constructor() {
+    this.booksArray = [];
+  }
+
+  getData() {
+    this.booksArray = JSON.parse(localStorage.getItem('books')) || [];
+  }
+
+  addBook(book) {
+    this.booksArray.push(book);
+  }
+
+  remove(book) {
+    booksArray = booksArray.filter(
+      (bookItem) => bookItem.id.toString() !== book.id
+    );
+  }
+
+  saveData() {
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+  }
 }
+
+const bookStore = new BookStore();
+bookStore.getData();
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const book1 = new Book(titleInput.value, authorInput.value);
-  console.log(book1);
+  const book = new Book(titleInput.value, authorInput.value);
+  bookStore.addBook(book);
+  bookStore.saveData();
 });
-
