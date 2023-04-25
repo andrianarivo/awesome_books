65b53cacec1e33900008c71cb49eade32cb85a8a
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-25 17:43:21 +0330

    Add comments to class methods and  build render method and write remove method

--- js/script-class.js
+++ js/script-class.js
@@ -15,23 +15,39 @@ class BookStore {
   constructor() {
     this.booksArray = [];
   }
-
+  // Get data from localstorage
   getData() {
     this.booksArray = JSON.parse(localStorage.getItem('books')) || [];
   }
-
+  // Add book to booksArray
   addBook(book) {
     this.booksArray.push(book);
   }
-
-  remove(book) {
+  // Remove book from booksArray
+  remove(id) {
     booksArray = booksArray.filter(
-      (bookItem) => bookItem.id.toString() !== book.id
+      (bookItem) => Number(bookItem.id) !== Number(id)
     );
   }
-
+  // Set data to localstorage
   saveData() {
     localStorage.setItem('books', JSON.stringify(this.booksArray));
+  }
+  // Show books in DOM
+  render() {
+    let content = '';
+    this.booksArray.forEach((book) => {
+      content += `
+    <li class="w-50 mx-auto">
+      <div>
+        <h3>${book.title}</h3>
+        <h4>${book.author}</h4>
+        <button type="button" id="${book.id}" class="remove-btn">Remove</button>
+      </div>
+      <hr>
+    </li>`;
+    });
+    return content;
   }
 }
 
@@ -39,8 +55,15 @@ const bookStore = new BookStore();
 bookStore.getData();
 
 addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   const book = new Book(titleInput.value, authorInput.value);
   bookStore.addBook(book);
   bookStore.saveData();
+  booksContainer.innerHTML = bookStore.render();
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    bookStore.remove(e.target.id);
+  }
 });
