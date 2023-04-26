0e7f47336126d6ad66ec204d5b71a7017511d259
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-26 11:54:17 +0330

    clean code js

--- js/script-class.js
+++ js/script-class.js
@@ -25,7 +25,7 @@ class BookStore {
   // Remove book from booksArray
   remove(id) {
     this.booksArray = this.booksArray.filter(
-      (bookItem) => Number(bookItem.id) !== Number(id),
+      (bookItem) => Number(bookItem.id) !== Number(id)
     );
   }
 
